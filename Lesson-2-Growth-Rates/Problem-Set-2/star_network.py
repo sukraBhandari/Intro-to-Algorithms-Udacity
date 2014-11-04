c98af135eaaef3fc71ef713456cb866f22979f4b
sukraBhandari <sukrab@yahoo.com>
2014-11-04 11:41:05 -0800

    Create star_network.py

---
+++ Lesson-2-Growth-Rates/Problem-Set-2/star_network.py
@@ -0,0 +1,9 @@
+# Write a program that returns the number of edges
+# in a star network that has `n` nodes 
+#
+
+def star_network(n):
+    # return number of edges
+    return n-1
+
+print star_network(5)
